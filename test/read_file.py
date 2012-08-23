9f1feecdf0487a9f4f9f41ea30a4296381a2a380
Fabrice Albert <falbert@ise.com>
2012-08-23 15:55:01 -0400

    f

--- test/read_file.py
+++ test/read_file.py
@@ -8,14 +8,21 @@ myfile = open ("file","r+")
 
 
 mylist = []
+router_type = []
 
 for line in myfile.readlines():
     mylist.append(line)
-
+    i=line.rstrip().split()
+    router_type.append(i)
+    
+    
 print mylist
+print router_type
+print router_type[1][1]
 
 for router in mylist:
     print router
+    
 
 
 
