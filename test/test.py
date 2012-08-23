fb401a4b3c9c6e5f9bbf4dd11004c790e6104cbd
Fabrice Albert <falbert@ise.com>
2012-08-23 14:55:25 -0400

    updt

--- test/test.py
+++ test/test.py
@@ -1,16 +1,21 @@
 import paramiko
 import sys
 import getpass
+import telnetlib
+
 
 
     
-user = raw_input("enter user")
-pwd = getpass.getpass()
+#user = raw_input("enter user")
+#pwd = getpass.getpass()
+user = 'albefab'
+pwd = 'PorscH927'
+
 host = '172.21.1.1'
 cmd = "sh proc cpu"
 print user
 print pwd
-
+print host
 
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
